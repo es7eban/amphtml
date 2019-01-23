402967d5cc0baf04064e98a4e7c64c27452ffca9
marcoaraujo <marco@digidip.net>
2019-01-23 16:56:12 +0100

    some tests for url builder

--- extensions/amp-digidip/0.1/test/mockData.js
+++ extensions/amp-digidip/0.1/test/mockData.js
@@ -1,17 +1,16 @@
 export const mockData = [
+
   {
-    publisherId: 'stylebudget',
-    useWorddip: '0',
-    newTab: '1',
-    encodedXhrCredentials: 'werqwer34ffG55gH4',
-    hostsIgnore: '',
-    readingWordsExclude: '',
-    elementClickhandler: '',
-    elementClickhandlerAttribute: '',
-    elementIgnoreAttribute: '',
-    elementIgnorePattern: '',
-    elementIgnoreConsiderParents: '',
-    href: '',
-    finaUrl: '...',
+    publisherId: 'hardware',
+    href: 'http://www.cdiscount.com/informatique/logiciels-informatique/windows-8-1-pro-licence-1-pc-32-64-bit/f-1070722-gam0885370716412.html?idOffre=39441226#mpos=4|mp',
+    finaUrl: 'http://hardware.digidip.net/visit?url=http%3A%2F%2Fwww.cdiscount.com%2Finformatique%2Flogiciels-informatique%2Fwindows-8-1-pro-licence-1-pc-32-64-bit%2Ff-1070722-gam0885370716412.html%3FidOffre%3D39441226%23mpos%3D4%7Cmp&currurl=https%3A%2F%2Fforum.hardware.fr%2Fhfr%2FWindowsSoftware%2Fwindows-8%2Fachat-license-cdiscount-sujet_340739_1.htm',
   },
+
+  {
+    publisherId: 'mobot',
+    href: 'http://best-zenith-replica.asamenter.com/',
+    finaUrl: 'http://mobot.digidip.net/visit?url=http%3A%2F%2Fbest-zenith-replica.asamenter.com%2F&ppref=http%3A%2F%2Fwww.mobot.net%2F&currurl=http%3A%2F%2Fwww.mobot.net%2Freview-omega-planet-ocean-ultimate-replica-watch',
+  }
+
+
 ];
