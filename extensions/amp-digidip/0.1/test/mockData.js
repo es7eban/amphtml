d55c2630d971e18acf8ba3afb27cac5c7bbaa8d8
Esteban <esteban@digidip.net>
2019-01-23 15:37:57 +0100

    - mocked data for testing

---
+++ extensions/amp-digidip/0.1/test/mockData.js
@@ -0,0 +1,17 @@
+export const mockData = [
+  {
+    publisherId: 'stylebudget',
+    useWorddip: '0',
+    newTab: '1',
+    encodedXhrCredentials: 'werqwer34ffG55gH4',
+    hostsIgnore: '',
+    readingWordsExclude: '',
+    elementClickhandler: '',
+    elementClickhandlerAttribute: '',
+    elementIgnoreAttribute: '',
+    elementIgnorePattern: '',
+    elementIgnoreConsiderParents: '',
+    href: '',
+    finaUrl: '...',
+  },
+];
