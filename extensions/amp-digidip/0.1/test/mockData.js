faa9d4fbe6e9337e036efaf6c9fb8a5c6cba3902
Esteban <esteban@digidip.net>
2019-01-24 10:37:55 +0100

    Fixed mock data for testing

--- extensions/amp-digidip/0.1/test/mockData.js
+++ extensions/amp-digidip/0.1/test/mockData.js
@@ -2,26 +2,22 @@ export const mockData = [
 
   {
     publisherId: 'hardware',
-    cururl: 'http://www.mobot.net/review-omega-planet-ocean-ultimate-replica-watch',
+    currUrl: 'https://forum.hardware.fr/hfr/WindowsSoftware/windows-8/achat-license-cdiscount-sujet_340739_1.htm',
     href: 'http://www.cdiscount.com/informatique/logiciels-informatique/windows-8-1-pro-licence-1-pc-32-64-bit/f-1070722-gam0885370716412.html?idOffre=39441226#mpos=4|mp',
     finaUrl: 'http://hardware.digidip.net/visit?url=http%3A%2F%2Fwww.cdiscount.com%2Finformatique%2Flogiciels-informatique%2Fwindows-8-1-pro-licence-1-pc-32-64-bit%2Ff-1070722-gam0885370716412.html%3FidOffre%3D39441226%23mpos%3D4%7Cmp&currurl=https%3A%2F%2Fforum.hardware.fr%2Fhfr%2FWindowsSoftware%2Fwindows-8%2Fachat-license-cdiscount-sujet_340739_1.htm',
   },
-
   {
     publisherId: 'mobot',
-    currurl: 'http://www.mobot.net/review-omega-planet-ocean-ultimate-replica-watch',
-    ppref: 'http://www.mobot.net/',
+    currUrl: 'http://www.mobot.net/review-omega-planet-ocean-ultimate-replica-watch',
+    ppRef: 'http://www.mobot.net/',
     href: 'http://best-zenith-replica.asamenter.com/',
     finaUrl: 'http://mobot.digidip.net/visit?url=http%3A%2F%2Fbest-zenith-replica.asamenter.com%2F&ppref=http%3A%2F%2Fwww.mobot.net%2F&currurl=http%3A%2F%2Fwww.mobot.net%2Freview-omega-planet-ocean-ultimate-replica-watch',
   },
-
   {
     publisherId: 'telegraph',
-    currurl: 'https://www.telegraph.co.uk/money/future-of-money/local-branch-has-gone-dont-like-online-banking-options/',
-    ppref: 'https://www.telegraph.co.uk/money/consumer-affairs/cashless-britain-local-branch-has-gone-now-goodbye-atm/',
+    currUrl: 'https://www.telegraph.co.uk/money/future-of-money/local-branch-has-gone-dont-like-online-banking-options/',
+    ppRef: 'https://www.telegraph.co.uk/money/consumer-affairs/cashless-britain-local-branch-has-gone-now-goodbye-atm/',
     href: 'http://postoffice.co.uk/branch-banking-services',
     finaUrl: 'http://telegraph.digidip.net/visit?url=http%3A%2F%2Fpostoffice.co.uk%2Fbranch-banking-services&ppref=https%3A%2F%2Fwww.telegraph.co.uk%2Fmoney%2Fconsumer-affairs%2Fcashless-britain-local-branch-has-gone-now-goodbye-atm%2F&currurl=https%3A%2F%2Fwww.telegraph.co.uk%2Fmoney%2Ffuture-of-money%2Flocal-branch-has-gone-dont-like-online-banking-options%2F',
-  }
-
-
+  },
 ];
